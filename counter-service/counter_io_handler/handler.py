65e2ee0ef65da585c8fd93c3b63c6c5b52fbd2f1
Nadav Salman <nadavops@gmail.com>
2024-11-15 11:01:10 +0200

    Update stdout to include counter file path

--- counter-service/counter_io_handler/handler.py
+++ counter-service/counter_io_handler/handler.py
@@ -13,12 +13,12 @@ class CounterIOHandler:
         if os.path.exists(self.file_path):
             with open(self.file_path, "r") as f:
                 try:
-                    # current_app.logger.info("Successfully read counter file")
-                    print("Successfully read counter file")
+                    # current_app.logger.info("Successfully read counter file {self.file_path}")
+                    print("Successfully read counter file {self.file_path}")
                     return int(f.read()) # str -> int ...
                 except Exception as e:
-                    # current_app.logger.info(f"Failed to read counter file, error : {ValueError}")
-                    print(f"Failed to read counter file, error : {ValueError}")
+                    # current_app.logger.info(f"Failed to read counter file {self.file_path}, error : {ValueError}")
+                    print(f"Failed to read counter file {self.file_path}, error : {ValueError}")
                     return 0
         return 0
 
@@ -26,8 +26,8 @@ class CounterIOHandler:
         try:
             with open(self.file_path, "w") as f:
                 f.write(str(value))
-            # current_app.logger.info(f"Successfully wrote value {value} to counter file")
-            print(f"Successfully wrote value {value} to counter file")
+            # current_app.logger.info(f"Successfully wrote value {value} to counter file {self.file_path}")
+            print(f"Successfully wrote value {value} to counter file {self.file_path}")
         except Exception as e:
-            # current_app.logger.error(f"Failed to write to counter file, error: {str(e)}")
-            print(f"Failed to write to counter file, error: {str(e)}")
+            # current_app.logger.error(f"Failed to write to counter file {self.file_path}, error: {str(e)}")
+            print(f"Failed to write to counter file {self.file_path}, error: {str(e)}")
